91a3bcb70a850bf2a36cb19c8f59f9e109ee89ca
Hans Cappelle <hans@roov.app>
2025-03-15 19:40:13 +0100

    added screenshots

--- app/src/main/java/be/hcpl/android/aar/mvp/MvpActivity.kt
+++ app/src/main/java/be/hcpl/android/aar/mvp/MvpActivity.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import be.hcpl.android.aar.common.AppScaffold
